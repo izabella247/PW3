8b079c4533160ceaac9628c5ca6c79e800604c2b
izabella247 <izabellaherculano06@gmail.com>
2026-05-28 20:24:29 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("formCadastro");
+const mensagem = document.getElementById("mensagem");
+
+form.addEventListener("submit", async function(event) {
+     event.preventDefault();
+
+     const nome = document.gentelementbyid("nome").value;
+
+}
